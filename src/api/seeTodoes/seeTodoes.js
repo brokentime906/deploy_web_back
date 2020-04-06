042a89e8cc917ccf16f6cf25f10bd09f79964f07
brokentime906 <brokentime906@gmail.com>
2020-04-06 09:21:34 +0900

    todo list order by desc

--- src/api/seeTodoes/seeTodoes.js
+++ src/api/seeTodoes/seeTodoes.js
@@ -3,7 +3,7 @@ import { prisma } from "../../../generated/prisma-client";
 export default {
   Query: {
     seeTodoes: async (_, args, __) => {
-      return await prisma.todoes();
+      return await prisma.todoes({ orderBy: "createdAt_DESC" });
     },
   },
 };
